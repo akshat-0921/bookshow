46d1ff604513aed965d24764fcc9c075d70e2eb0
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-06-21 14:58:08 +0530

    tailwind config

---
+++ client/src/pages/MyBookings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MyBookings(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default MyBookings;
---
+++ client/src/pages/SeatLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SeatLayout(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default SeatLayout;
