1e5b41cf1ae9e82aba9cdfc033346b8251cb4c7b
Sumitsingh74 <129742295+Sumitsingh74@users.noreply.github.com>
2025-06-22 17:31:34 +0530

    vercel json added

--- server/server.js
+++ server/server.js
@@ -3,19 +3,24 @@ import cors from "cors";
 import 'dotenv/config'
 import connectBD from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import {serve} from "inngest/express";
+import {inngest,functions} from "./inngest/index.js";
 
 const app = express();
 await connectBD();
+
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(ClerkMiddleware());
+app.use(clerkMiddleware());
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 
 
